0d3126ee38998013f63323a7bdfff9abef24607e
PervizVeliyev <veliyevperviz1@gmail.com>
2023-02-18 15:20:10 +0400

    addTask and updateTask service

--- src/main/java/com/todolist/todolist/controller/TaskController.java
+++ src/main/java/com/todolist/todolist/controller/TaskController.java
@@ -3,10 +3,8 @@ package com.todolist.todolist.controller;
 import com.todolist.todolist.dao.entity.Task;
 import com.todolist.todolist.service.TaskService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,10 +14,20 @@ import java.util.List;
 public class TaskController {
   private final TaskService taskService;
 
-  @GetMapping("/userid{userid}")
+  @GetMapping("/userid/{userid}")
   public List<Task> getTasksByUserId(@PathVariable Long userid) {
     return taskService.getTasksByUserId(userid);
   }
 
+  @PostMapping("/addTask/{userId}")
+  public ResponseEntity<Task> createTask(@PathVariable Long userId, @RequestBody Task task) {
+    Task createdTask = taskService.createTaskById(userId, task);
+    return ResponseEntity.ok(createdTask);
+  }
 
+  @PutMapping("/{taskId}")
+  public ResponseEntity<Task> updateTask(@PathVariable Long taskId, @RequestBody Task task) {
+    Task updatedTask = taskService.updateTask(taskId, task);
+    return ResponseEntity.ok(updatedTask);
+  }
 }
--- src/main/java/com/todolist/todolist/service/TaskService.java
+++ src/main/java/com/todolist/todolist/service/TaskService.java
@@ -4,6 +4,7 @@ import com.todolist.todolist.dao.entity.Task;
 import com.todolist.todolist.dao.entity.User;
 import com.todolist.todolist.dao.repository.TaskRepository;
 import com.todolist.todolist.dao.repository.UserRepository;
+import com.todolist.todolist.exception.TaskNotFoundException;
 import com.todolist.todolist.exception.UserNotFoundException;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -24,5 +25,26 @@ public class TaskService {
     Optional<User> optionalUser = userRepository.findById(id);
     User user = optionalUser.orElseThrow(() -> new UserNotFoundException("user not found"));
     return user.getTasks();
+  }
+
+  public Task createTaskById(Long userId, Task task) {
+    Optional<User> userOptional = userRepository.findById(userId);
+    User user = userOptional.orElseThrow(() -> new UserNotFoundException(String.format("User with %d not found", userId)));
+    task.setUser(user);
+    return taskRepository.save(task);
+  }
+
+  public Task updateTask(Long id, Task newTask) {
+    return taskRepository.findById(id)
+            .map(task -> {
+              task.setTaskName(newTask.getTaskName());
+              task.setTaskStatus(newTask.getTaskStatus());
+              task.setDescription(newTask.getDescription());
+              task.setPhoto(newTask.getPhoto());
+              task.setTaskSortType(newTask.getTaskSortType());
+              task.setTaskDeadlineDate(newTask.getTaskDeadlineDate());
+              return taskRepository.save(task);
+            })
+            .orElseThrow(() -> new TaskNotFoundException(String.format("Task with %d not found", id)));
   }
 }
