73043b64777b18a8942bc82607e95ee483f8e9a5
vusalahasanova01 <vusalahasanova423@gmail.com>
2023-02-18 11:17:08 +0400

    added getTasksById service

--- src/main/java/com/todolist/todolist/controller/TaskController.java
+++ src/main/java/com/todolist/todolist/controller/TaskController.java
@@ -1,5 +1,25 @@
 package com.todolist.todolist.controller;
 
+import com.todolist.todolist.dao.entity.Task;
+import com.todolist.todolist.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
+@RequiredArgsConstructor
 public class TaskController {
+  private final TaskService taskService;
+
+  @GetMapping("/userid{userid}")
+  public List<Task> getTasksByUserId(@PathVariable Long userid) {
+    return taskService.getTasksByUserId(userid);
+  }
+
 
 }
--- src/main/java/com/todolist/todolist/service/TaskService.java
+++ src/main/java/com/todolist/todolist/service/TaskService.java
@@ -1,4 +1,28 @@
 package com.todolist.todolist.service;
 
+import com.todolist.todolist.dao.entity.Task;
+import com.todolist.todolist.dao.entity.User;
+import com.todolist.todolist.dao.repository.TaskRepository;
+import com.todolist.todolist.dao.repository.UserRepository;
+import com.todolist.todolist.exception.UserNotFoundException;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+  private final UserRepository userRepository;
+  private final TaskRepository taskRepository;
+
+  public List<Task> getTasksByUserId(Long id) {
+    //List<Task> tasks;
+    Optional<User> optionalUser = userRepository.findById(id);
+    User user = optionalUser.orElseThrow(() -> new UserNotFoundException("user not found"));
+    return user.getTasks();
+  }
 }
