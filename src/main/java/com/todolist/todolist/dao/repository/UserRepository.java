2648afd11498ece4f0a9d5133e2603abe8075f90
talibnabi <104730536+talibnabi@users.noreply.github.com>
2023-03-01 16:49:16 +0400

    Update UserRepository.java

--- src/main/java/com/todolist/todolist/dao/repository/UserRepository.java
+++ src/main/java/com/todolist/todolist/dao/repository/UserRepository.java
@@ -8,5 +8,6 @@ public interface UserRepository extends JpaRepository<User,Long> {
     User findByEmail(String email);
 
     User findByVerificationCode(String code);
-
+    
+    void deleteUserByEmail(String email);
 }
