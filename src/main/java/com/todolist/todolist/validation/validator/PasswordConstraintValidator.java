9cd4805212d0d484f2d2066452df31d6005e4f6f
talibnabi <104730536+talibnabi@users.noreply.github.com>
2023-03-01 18:12:53 +0400

    Update PasswordConstraintValidator.java

--- src/main/java/com/todolist/todolist/validation/validator/PasswordConstraintValidator.java
+++ src/main/java/com/todolist/todolist/validation/validator/PasswordConstraintValidator.java
@@ -21,13 +21,7 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
     public boolean isValid(String password, ConstraintValidatorContext context) {
         PasswordValidator validator = new PasswordValidator(Arrays.asList(
                 new LengthRule(8, 30),
-                new UppercaseCharacterRule(1),
-                new DigitCharacterRule(1),
-                new SpecialCharacterRule(1),
-                new NumericalSequenceRule(3, false),
-                new AlphabeticalSequenceRule(3, false),
-                new QwertySequenceRule(3, false),
-                new WhitespaceRule()));
+              ));
 
         RuleResult result = validator.validate(new PasswordData(password));
         if (result.isValid()) {
